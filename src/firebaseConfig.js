85fbf50c2246f4cef43d13663a522637468ba9fb
om-arya <om.arya0577@gmail.com>
2026-02-15 20:17:29 -0500

    Finish merge

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuigYOf4q-piuPvpP3L4_RlMjbF6NX8IA",
+  authDomain: "opensourcery-website-backend.firebaseapp.com",
+  projectId: "opensourcery-website-backend",
+  storageBucket: "opensourcery-website-backend.firebasestorage.app",
+  messagingSenderId: "122874880770",
+  appId: "1:122874880770:web:6f59ec5fcb12b57a9c981b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
