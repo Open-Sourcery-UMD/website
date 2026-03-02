c7243725cfadcd4695e2077aed92c75d03336ab5
om-arya <om.arya0577@gmail.com>
2026-03-02 17:56:36 -0500

    Update FIREBASE_SERVICE_ACCOUNT_KEY logic

--- scripts/lib/firebase-admin.ts
+++ scripts/lib/firebase-admin.ts
@@ -1,8 +1,14 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
+const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+
+if (!base64) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY is not set");
+}
+
 const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}"
+  Buffer.from(base64, "base64").toString("utf-8")
 );
 
 if (getApps().length === 0) {
